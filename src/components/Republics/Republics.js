78a9ddd57e3954a0c18a44c8d723224f4aafcb4e
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2022-05-12 14:21:36 +0500

    create pathFunc

--- src/components/Republics/Republics.js
+++ src/components/Republics/Republics.js
@@ -1,9 +1,9 @@
 import classes from "./Republics.module.css";
 import React from "react";
-import { MyButton, MyInput } from "../MyUI/MyUI";
+import { MyButton, MyInput, pathFunc } from "../MyUI/MyUI";
 import { Link } from "react-router-dom";
 
-const Republics = ({ data, changeBckg, breadcrumb }) => {
+const Republics = ({ data, changeBckg }) => {
     return (
         <div className={classes.Republics}>
             <div className={classes.row_1}>
@@ -28,7 +28,7 @@ const Republics = ({ data, changeBckg, breadcrumb }) => {
                 <MyInput placeholder="Search" />
             </div>
             <div className={classes.row_3}>
-                <small>{breadcrumb}</small>
+                <small>{pathFunc()}</small>
             </div>
         </div>
     );
